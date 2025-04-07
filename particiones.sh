f97d905bd3b9250e01fcd71e3bf5d3f069c80998
Aleksandr Kosenko <aleksanko14@gmail.com>
2025-04-07 09:58:23 +0200

    Añadido script

---
+++ particiones.sh
@@ -0,0 +1,9 @@
+#Script particionado disco con sgdisk
+#Autores: Sergio Jiménez, Aleksandr Kosenko, Roberto Martín
+
+read -p "Indique el disco a particionar: " disco
+read -p "Introduzca las particiones a realizar: " final
+read -p "Indique tamaño de la partción en megas: " size
+
+for particion in $(seq 1 $final); do sudo sgdisk --new=0:0:+$size $disco > /dev/null; done
+echo "Particiones realizadas"
