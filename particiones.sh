36c75ceee198da6651be94228e4e0a8ee84777a4
aleksanko2025 <aleksanko14@gmail.com>
2025-04-07 12:42:34 +0200

    Update particiones.sh

--- particiones.sh
+++ particiones.sh
@@ -5,5 +5,5 @@ read -p "Indique el disco a particionar: " disco
 read -p "Introduzca las particiones a realizar: " final
 read -p "Indique tamaño de la partción en megas: " size
 
-for particion in $(seq 1 $final); do sudo sgdisk --new=0:0:+$size $disco > /dev/null; done
+for particion in $(seq 1 $final); do sudo sgdisk --new=0:0:+$size /dev/$disco > /dev/null; done
 echo "Particiones realizadas"
