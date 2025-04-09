b2d98b0e752e7cd23c688c1eebcdfd893c137a4c
aleksanko2025 <aleksanko14@gmail.com>
2025-04-09 10:13:47 +0200

    Update particiones.sh
    Falta añadir la comprobación de si está instalado el paquete dialog

--- particiones.sh
+++ particiones.sh
@@ -3,6 +3,7 @@
 #Autores: Sergio Jiménez, Aleksandr Kosenko, Roberto Martín
 
 #Comprobamos que eres root
+function soy_root(){
 for i in $(seq 0 10 100)
 do
 sleep 0.1
@@ -10,42 +11,60 @@ echo $i | dialog --gauge "Comprobando que eres root..." 0 0 0
 done
 clear
 if [[ ! $UID -eq 0 ]]; then
-dialog --msgbox "Este script debe ejecutarse como root, vuelve a entrar como superusuario" 0 0 
+dialog --msgbox "Este script debe ejecutarse como root, vuelve a entrar como usuario root
+" 0 0 
 clear
 exit
 fi
+}
 
-while true; do  
-read -p "Indique el disco a particionar: " disco  
+function select_disk(){
+while true; do
+read -p "Indique el disco a particionar: " disco
 
-#Verificamos si el disco existe  
-if ! lsblk | grep -q $disco; then  
-    echo -e "El disco dev/$disco no existe"  
-else  
-    break  
-fi  
-done  
+#Verificamos si el disco existe
+if ! lsblk | grep -q $disco; then 
+   echo -e "El disco dev/$disco no existe"
+else
+  break
+fi
+done
+}
 
-while true; do  
-read -p "Introduzca las particiones a realizar: " num_part  
+function num_particiones(){
+while true; do
+read -p "Introduzca las particiones a realizar: " num_part
 
-#Comprobamos el numero de particiones  
-if [[ $num_part -gt 128 || $num_part -lt 1]]; then  
-    echo "Número de particiones no válido"  
-else  
-    break  
-fi  
-done  
+#Comprobamos el numero de particiones
+if [[ $num_part -gt 128 || $num_part -lt 1 ]]; then
+  echo "Número de particiones no válido"
+else 
+  break
+fi
+done
+}
 
-while true; do  
-read -p "Indique tamaño de la partición en megas (E). 512M): " size  
+function part_size(){
+while true; do
+read -p "Indique tamaño de la partción en megas (Ej. 512M): " size
 
-#Confirmamos que este bien escrito el tamaño  
-if [[ ! "$size" =~ ^[0-9]{1,5}M$ ]]; then  
-   echo "El tamaño no es válido"  
-else  
-    break  
-fi  
-done  
+#Confirmamos que este bien escrito el tamaño
+if [[ ! "$size" =~ ^[0-9]{1,5}M$ ]]; then
+  echo "El tamaño no es válido"
+else
+  break
+fi
+done
+}
 
-for partition in $(seq 1 $num_part); do sudo sgdisk --new-0:0:+$size /dev/$disco > /dev/null; done
+function ejecucion(){
+for particion in $(seq 1 $num_part); do sudo sgdisk --new=0:0:+$size /dev/$disco > /dev/null; done
+echo "Particiones realizadas"
+exit
+}
+
+soy_root
+select_disk
+num_particiones
+part_size
+ejecucion
